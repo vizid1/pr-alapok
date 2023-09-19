32b4adc79668fb05f06c6bbad39e1b7e3e6ba30f
vizid1 <124770583+vizid1@users.noreply.github.com>
2023-09-19 21:32:24 +0200

    Update feladat_002.py

--- 3-4-ora/feladat_002.py
+++ 3-4-ora/feladat_002.py
@@ -5,6 +5,14 @@ Kérjünk be a billentyűzetről két egész számot és
 """
 szam_1 = int(input("Kérek egy számot: "))
 szam_2 = int(input("Kérek egy másik számot: "))
+szam_3 = float(szam_1)
+
 print(f"A megadott két szám összege: {szam_1 + szam_2}")
 összeg = szam_1 + szam_2
 print(f"A megadott két szám összege: {összeg}")
+
+print(f"A szam_1 változó típusa: {type(szam_1)}")
+print(f"A szam_2 változó típusa: {type(szam_2)}")
+print(f"A szam_3 változó típusa: {type(szam_3)}")
+
+print(f"A szam_3 változó értéke: {szam_3}")
