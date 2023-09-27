9fd6bdcf00c59fc3320c94b1534662252cb33bf1
vizid1 <124770583+vizid1@users.noreply.github.com>
2023-09-27 19:11:54 +0200

    Add files via upload

---
+++ ujmappa/hazi_feladat_1.py
@@ -0,0 +1,7 @@
+valasz = input(f'Jó a mai napod? (igen/nem) ')
+if valasz == 'igen':
+	print(f'Az szuper.')
+elif valasz == 'nem':
+	print(f'Az szomorú.')
+else:
+	print(f'Sajnos nem értem a válaszodat!')
